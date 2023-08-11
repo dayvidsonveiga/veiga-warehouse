2f7c8061c72abe5d36d89e6b923d031bef288223
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-08-11 16:55:13 -0300

    product-ms/Add spring-validator and modelmapper

--- product-ms/src/main/java/br/com/veiga/productms/dto/ProductDTO.java
+++ product-ms/src/main/java/br/com/veiga/productms/dto/ProductDTO.java
@@ -2,13 +2,21 @@ package br.com.veiga.productms.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 @Data
 public class ProductDTO {
 
+    @NotBlank
     private String name;
 
+    @NotBlank
+    @Size(min = 50)
     private String description;
 
+    @Positive
     private double price;
 
     private boolean available;
