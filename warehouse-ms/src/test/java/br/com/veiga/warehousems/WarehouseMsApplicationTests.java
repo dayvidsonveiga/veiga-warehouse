5cb00fe09762161fde29eab0b1eca48bdf78b803
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-08-11 15:54:06 -0300

    Add project warehouse-ms

---
+++ warehouse-ms/src/test/java/br/com/veiga/warehousems/WarehouseMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.veiga.warehousems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
