58925072e60ce09347b096405d85904cb8bdcd8a
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-10-23 22:26:04 -0300

    product-ms/Add shouldCreateProduct controller test

---
+++ product-ms/src/test/java/br/com/veiga/productms/controller/ProductControllerTest.java
@@ -0,0 +1,46 @@
+package br.com.veiga.productms.controller;
+
+import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
+import br.com.veiga.productms.dto.ProductDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @BeforeAll
+    public static void setUp() {
+        FixtureFactoryLoader.loadTemplates("br.com.veiga.productms.fixture");
+    }
+
+    @Test
+    public void shouldCreateProduct() throws Exception {
+        ProductDTO request = Fixture.from(ProductDTO.class).gimme("valid");
+        String content = mapper.writeValueAsString(request);
+
+        mvc.perform(post("/products")
+                        .header(AUTHORIZATION, "Bearer foo")
+                        .contentType(APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isCreated());
+    }
+
+}
